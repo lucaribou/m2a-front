e577fc9f6c985379d4ff5552e11c0092bee52718
Lucas SAISSI <lucas.saissi@gmail.com>
2024-06-24 10:42:33 +0200

    feat: create .env file

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import dotenv from 'dotenv';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  define: {
-    'import.meta.env': {
-      VITE_BACKEND_HOST: process.env.VITE_BACKEND_HOST,
-      VITE_BACKEND_PORT: process.env.VITE_BACKEND_PORT
-    }
-  }
-});
+})
