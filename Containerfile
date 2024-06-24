6fc7abe82153ca325d39f24284be580255ea3255
Lucas SAISSI <lucas.saissi@gmail.com>
2024-06-24 10:35:17 +0200

    feat: use process.env

--- Containerfile
+++ Containerfile
@@ -10,11 +10,4 @@ COPY . .
 RUN npm run build
 
 FROM docker.io/nginx:1.25.5-alpine as production
-
-ARG VITE_BACKEND_HOST
-ARG VITE_BACKEND_PORT
-
-ENV VITE_BACKEND_HOST=$VITE_BACKEND_HOST
-ENV VITE_BACKEND_PORT=$VITE_BACKEND_PORT
-
 COPY --from=build /app/dist /usr/share/nginx/html
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import dotenv from 'dotenv';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  define: {
+    'import.meta.env': {
+      VITE_BACKEND_HOST: process.env.VITE_BACKEND_HOST,
+      VITE_BACKEND_PORT: process.env.VITE_BACKEND_PORT
+    }
+  }
+});
