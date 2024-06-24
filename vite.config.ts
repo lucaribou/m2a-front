4d1686e09060761fb78aff947194dc27210a227a
Lucas SAISSI <lucas.saissi@gmail.com>
2024-06-24 10:19:04 +0200

    feat: add build arg

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import dotenv from 'dotenv';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
-dotenv.config();
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  define: {
-    'process.env': process.env
-  }
-});
+})
